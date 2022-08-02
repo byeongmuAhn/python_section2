522aec75fcf26a6dca41a9abfbabb9e74310293b
byeongmuAhn <bmahn@knou.ac.kr>
2022-08-02 16:18:36 +0900

    두번째 수정 v1.1

--- download2-2-1.py
+++ download2-2-1.py
@@ -5,6 +5,8 @@ import urllib.request as dw
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
+print('하이')
+
 imgUrl ="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjAyMjZfMjE2%2FMDAxNjQ1ODQ5NjAxNTA1.2lqntDk_ocYTLckKZfARn_clTSuuwqgVFwGeqAeu9Vgg.paCSBMVeGWOsAy0f09MgSl3LvdYn-eeJKEfPhISbSaIg.JPEG.kwskorea52%2Fkb_buddies_2008-03.jpg"
 htmlURL ="http://google.com"
 
